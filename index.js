80a0c47e32bb9d7650aeed7d182fd80b0005444c
StasTerletskiy <stanislavterletskiy1@gmail.com>
2020-07-03 15:40:01 +0300

    Add map() prototype method to MyArray

--- index.js
+++ index.js
@@ -81,6 +81,20 @@ myArrayProto.filter = function(callback) {
 
 };
 
+myArrayProto.map = function(callback) {
+
+  const res = [];
+
+  for (let i = 0; i < this.length; ++i) {
+
+    res.push(callback(this[i], i, this));
+
+  }
+
+  return res;
+
+};
+
 
 MyArray.prototype = myArrayProto;
 
