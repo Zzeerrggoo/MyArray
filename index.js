d6cb86fb89eca20dab98c62d17f7d021a7aff472
StasTerletskiy <stanislavterletskiy1@gmail.com>
2020-07-03 15:39:45 +0300

    Add filter() prototype method to MyArray

--- index.js
+++ index.js
@@ -67,6 +67,19 @@ myArrayProto.join = function(separator = ',') {
 
 };
 
+myArrayProto.filter = function(callback) {
+
+  const res = [];
+
+  for (let i = 0; i < this.length; ++i) {
+
+    if (callback(this[i], i, this)) res.push(this[i]);
+
+  }
+
+  return res;
+
+};
 
 
 MyArray.prototype = myArrayProto;
