a0df1bb5d091b7260574c9058a43bba5c5c1dabc
StasTerletskiy <stanislavterletskiy1@gmail.com>
2020-07-03 15:39:10 +0300

    Add includes() prototype method to MyArray

--- index.js
+++ index.js
@@ -40,6 +40,18 @@ myArrayProto.find = function(callback) {
   }
 };
 
+myArrayProto.includes = function(searchElement, fromIndex = 0) {
+
+  fromIndex < 0 && (fromIndex = this.length + fromIndex);
+
+  for (let i = fromIndex; i < this.length; ++i) {
+
+    if (this[i] === searchElement) return true;
+  }
+
+  return false;
+};
+
 
 
 MyArray.prototype = myArrayProto;
